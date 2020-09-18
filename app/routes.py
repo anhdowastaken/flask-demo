e41ff1c911a7a8b3b159e6e28c07a48c33cabdf2
Anh Do <doducanh2710@gmail.com>
2020-09-18 09:10:38 +0700

    Demo login form with simple validation
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello, World!'
+	return render_template('base.html', title='Index')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, is-admin={}'.format(form.username.data, form.isAdmin.data))
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Login', form=form)
