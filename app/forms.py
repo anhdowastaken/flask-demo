2f59670dfa6156a8efd54a0a0a573d07053061af
Alex Do <alex@twofive25.com>
2020-09-21 15:19:29 +0700

    Demo upload and download file
    Signed-off-by: Alex Do <alex@twofive25.com>

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	isAdmin = BooleanField('Is Admin?')
 	submit = SubmitField('Login')
+
+class UploadLicenseForm(FlaskForm):
+	licenseFile = FileField('License File', validators=[FileRequired()])
+	submit = SubmitField('Upload')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, send_file
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
-from app.forms import LoginForm
-from app.models import User, role_required
+from app import app, db
+from app.forms import LoginForm, UploadLicenseForm
+from app.models import User, License, role_required
+from io import BytesIO
 
 @app.route('/')
 @app.route('/index')
@@ -16,6 +17,15 @@ def index():
 @role_required('admin')
 def admin():
 	return render_template('admin.html', title='Admin')
+
+@app.route('/sale')
+@login_required
+@role_required('sale')
+def sale():
+	form = UploadLicenseForm()
+	licenseFiles = License.query.all()
+
+	return render_template('sale.html', title='Sale', licenseFiles=licenseFiles, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -43,6 +53,8 @@ def login():
 		if form.isAdmin.data:
 			return redirect(url_for('admin'))
 		else:
+			if user.role == 'sale':
+				return redirect(url_for('sale'))
 			return redirect(url_for('index'))
 
 	return render_template('login.html', title='Login', form=form)
@@ -52,3 +64,28 @@ def logout():
 	# Use flash_login to logout
 	logout_user()
 	return redirect(url_for('index'))
+
+@app.route('/upload', methods=['POST'])
+@login_required
+@role_required('sale')
+def upload():
+	form = UploadLicenseForm()
+	if form.validate_on_submit():
+		f = form.licenseFile.data
+		licenseFile = License(content=f.read())
+		licenseFile.user_id = current_user.id
+		db.session.add(licenseFile)
+		db.session.commit()
+
+	return redirect(url_for('sale'))
+
+@app.route('/download/<license_id>', methods=['GET'])
+@login_required
+@role_required('sale')
+def download(license_id):
+	licenseFile = License.query.filter_by(id=license_id).first()
+	if licenseFile:
+		return send_file(BytesIO(licenseFile.content), attachment_filename='license', as_attachment=True)
+	else:
+		flash('License file is unavailable')
+		return redirect(url_for('sale'))
