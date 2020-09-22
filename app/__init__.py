23a15173d3797c8e54ff0538c068d586b4b34f57
Alex Do <alex@twofive25.com>
2020-09-22 10:05:04 +0700

    Ensure number of admin users is not greater than 1 when inserting
    Signed-off-by: Alex Do <alex@twofive25.com>

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 login.login_view = 'login'
 
@@ -19,25 +20,3 @@ else:
 	logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
 
 from app import routes, models
-
-def checkAdmin():
-	from app.models import User
-
-	adminUsers = User.query.filter_by(role='admin').all()
-	# Delete all admin users if number of admin users is greater than 1
-	if len(adminUsers) > 1:
-		app.logger.info('There are {} admin users in database'.format(len(adminUsers)))
-		app.logger.info('All of them will be deleted')
-		for u in adminUsers:
-			db.session.delete(u)
-			db.session.commit()
-	
-	# Add admin user if necessary
-	if len(adminUsers) != 1:
-		app.logger.info('Add new admin user with default password 1')
-		u = User(username='admin', role='admin')
-		u.set_password('1')
-		db.session.add(u)
-		db.session.commit()
-
-checkAdmin()
--- app/models.py
+++ app/models.py
@@ -3,15 +3,32 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import functools
 from flask import abort
 from flask_login import UserMixin, current_user
+from sqlalchemy.orm import validates
 from app import db
 from app import login
 
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
+	username = db.Column(db.String(64), index=True, unique=True, nullable=False)
 	password_hash = db.Column(db.String(128))
-	role = db.Column(db.String(32))
+	role = db.Column(db.Enum('admin', 'sale'), default='sale')
 	licenses = db.relationship('License', backref='uploader', lazy='dynamic')
+
+	# This solution below doesn't work
+	# because A check constraint cannot contain a subquery (sqlite.org/lang_createtable.html)
+	# __table_args__ = (
+	# 	db.CheckConstraint('(SELECT COUNT(*) FROM user WHERE role="admin") <= 1'),
+	# )
+	# Use this instead
+	@validates('role')
+	def validate_role(self, key, role):
+		if not role:
+			raise AssertionError('No role provided')
+
+		if User.query.filter_by(role='admin').count() >= 1 and role == 'admin':
+			raise AssertionError('Admin already existed')
+
+		return role
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
