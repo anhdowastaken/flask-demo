1b3dc00b3747c206753416716a4d77e825dea7e6
Alex Do <alex@twofive25.com>
2020-09-23 11:06:50 +0700

    Demo schedule cron job
    Signed-off-by: Alex Do <alex@twofive25.com>

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_apscheduler import APScheduler
 import os
 import logging
 
@@ -20,3 +21,15 @@ else:
 	logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
 
 from app import routes, models
+
+scheduler = APScheduler()
+@scheduler.task('cron', id='check_expired_license', minute='*')
+def check_expired_license():
+	ls = models.License.query.all()
+	logging.info('Number of licenses = {}'.format(len(ls)))
+
+# Avoid running job twice in debug mode
+# https://www.tfzx.net/article/2758814.html
+if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+	scheduler.init_app(app)
+	scheduler.start()
